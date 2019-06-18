b5f18e407f2a038bd8d98f91e97d70cc0d47cad0
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-06-18 16:50:44 -0400

    made grid for skills images

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -17,59 +17,73 @@ class Skills extends Component {
           maintain applications.
         </p>
         <p>My Tech Stack</p>
-        <img
-          className="images"
-          src="https://icongr.am/devicon/react-original.svg"
-          alt=""
-        />
-        <p>React</p>
-        <img
-          className="images"
-          src="https://cdn.freebiesupply.com/logos/large/2x/redux-logo-png-transparent.png"
-          alt=""
-        />
-        <p>Redux</p>
-        <img
-          className="images"
-          src="https://icongr.am/devicon/javascript-original.svg"
-          alt=""
-        />
-        <p>JavaScript</p>
-        <img
-          className="images"
-          src="https://icongr.am/devicon/postgresql-original.svg"
-          alt=""
-        />
-        <p>PostGresql</p>
-        <img
-          className="images"
-          src="https://icongr.am/devicon/html5-original.svg"
-          alt=""
-        />
-        <p>HTML5</p>
-        <img
-          className="images"
-          src="https://icongr.am/devicon/git-original.svg"
-          alt=""
-        />
-        <p>Git</p>
-        <img
-          className="images"
-          src="https://icongr.am/devicon/css3-original.svg"
-          alt=""
-        />
-        <p>CSS3</p>
-        <img
-          className="images"
-          src="https://icongr.am/devicon/nodejs-original-wordmark.svg"
-          alt=""
-        />
-        <br />
-        <img
-          className="images"
-          src="https://icongr.am/devicon/express-original-wordmark.svg"
-          alt=""
-        />
+        <div className="mainImagesContainer">
+          <div className="innerMainContainer">
+            <div className="row1Container">
+              <div className="row1">
+                <img
+                  className="images"
+                  src="https://icongr.am/devicon/react-original.svg"
+                  alt=""
+                />
+                <p>React</p>
+                <img
+                  className="images"
+                  src="https://cdn.freebiesupply.com/logos/large/2x/redux-logo-png-transparent.png"
+                  alt=""
+                />
+                <p>Redux</p>
+                <img
+                  className="images"
+                  src="https://icongr.am/devicon/javascript-original.svg"
+                  alt=""
+                />
+                <p>JavaScript</p>
+              </div>
+            </div>
+
+            <div className="row2">
+              <img
+                className="images"
+                src="https://icongr.am/devicon/postgresql-original.svg"
+                alt=""
+              />
+              <p>PostGresql</p>
+              <img
+                className="images"
+                src="https://icongr.am/devicon/html5-original.svg"
+                alt=""
+              />
+              <p>HTML5</p>
+              <img
+                className="images"
+                src="https://icongr.am/devicon/git-original.svg"
+                alt=""
+              />
+              <p>Git</p>
+            </div>
+
+            <div className="row3">
+              <img
+                className="images"
+                src="https://icongr.am/devicon/css3-original.svg"
+                alt=""
+              />
+              <p>CSS3</p>
+              <img
+                className="images"
+                src="https://icongr.am/devicon/nodejs-original-wordmark.svg"
+                alt=""
+              />
+              <br />
+              <img
+                className="images"
+                src="https://icongr.am/devicon/express-original-wordmark.svg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
