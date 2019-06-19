b9e67047355e81ad0754bfd41a1553767f9190ba
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-06-18 22:03:53 -0400

    fixed up the about us page and getting ready to deploy

--- src/Components/About.js
+++ src/Components/About.js
@@ -50,8 +50,8 @@ class About extends Component {
             Computer technician turned fullstack web developer. I have a love
             for technology and wanted to create and maintain applications. I
             entered A program called Pursuit and I learned How to be a full
-            stack Developer. Using React, Javascript, Express, SQL, Postgres and
-            node.js
+            stack Developer. Using React, Redux, Javascript, Express, SQL,
+            Postgres and node.js.
           </p>
         </div>
       </div>
